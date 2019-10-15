a55470e18a95b6f2bd72fcb65eeb7f70f74f5d63
Romario Gustave <romagustave10@gmail.com>
2019-10-15 03:55:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,5 +19,5 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   nil
-  directors = [{:name "Georges" :value }]
+  directors = [{:name "Georges" :value=> 1234567890}]
 end
